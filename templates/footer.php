c72111946b6a909e10e687062884b8e7032e49fd
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2013-09-14 20:01:33 -0700

    add header, footer colors and logo

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,23 @@
-<footer class="content-info container" role="contentinfo">
-  <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+        <div class="footer-logo"><img alt="Selkirk Mountain Helicopters" src="/assets/img/logo-white-160x150.png" width="160" height="150" /></div>
+        <div class="footer-address">
+          <p>530 Westside Road<br>
+          PO Box 2968<br>
+          Revelstoke, BC V0E 2S0<br>
+          <span class="footer-phone">250-837-2455</span><br>
+          <span class="footer-fax">250-837-4066</span><br>
+          <a href="mailto:info@smheli.com">info@smheli.com</a></p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 copyright">
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+      </div>
     </div>
   </div>
 </footer>
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img alt="<?php bloginfo('name'); ?>" src="/assets/img/logo-white-85x80.png" width="85" height="80"/></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
