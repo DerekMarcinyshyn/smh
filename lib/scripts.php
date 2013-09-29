4e5cc2cacfbeaa0fc6e9edbf5cb447c0235a7f92
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2013-09-28 22:30:35 -0700

    change background image and to not fixed position

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f218dfda5c59a485b3186574e2c004a0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0efe12c8863c11e0ee4f08a1f1936cdc');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
